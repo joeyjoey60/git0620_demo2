85331fc91f8a7ee608cad2376a1863046ff69c23
joey <2401318067@qq.com>
2019-06-20 15:08:18 +0800

    second

---
+++ src/main/java/com/joey/demo/controller/CommonController.java
@@ -0,0 +1,11 @@
+package com.joey.demo.controller;
+
+/**
+ * Created by zhou on 2019/6/20.
+ */
+public class CommonController {
+
+    public  void  test(){
+        System.out.println("hhhhhhhhhhh");
+    }
+}
